02cdc6d8290a3382c42ce93c04a315cf0d5e58a3
akshaya23789 <akshaya23789@gmail.com>
2026-03-04 00:04:29 +0530

    OOPS banner unsing multiple print statements

--- OOPSBannerAppUC2.java
+++ OOPSBannerAppUC2.java
@@ -1,4 +1,4 @@
-public class OOPSBannerAppUC2{
+public class OOPSBannerAppuc2{
 	public static void main(String[] args){
 		System.out.println("   ***   " + " " + "   ***   " + " " + "******   " + " " + "   ***** ");
 		System.out.println(" **   ** " + " " + " **   ** " + " " + "**   **  " + " " + " **      ");
